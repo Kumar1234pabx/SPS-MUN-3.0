c888c40f640e83cf96bef697783de2e67152fee4
Kumar1234pabx <tobimadara472@gmail.com>
2026-05-15 05:07:31 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+console.log("SPS MUN 3.0 Loaded");
+
+document.querySelectorAll("a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        console.log("Navigating...");
+
+    });
+
+});
